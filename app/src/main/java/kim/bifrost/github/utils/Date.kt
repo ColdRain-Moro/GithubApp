51bf7c6a6266d55ee575e03f054482a73cc2b3e2
Rain <69996135+ColdRain-Moro@users.noreply.github.com>
2022-07-25 21:15:22 +0800

    :bug: 修复Trace日期显示问题 & 更新README

--- app/src/main/java/kim/bifrost/github/utils/Date.kt
+++ app/src/main/java/kim/bifrost/github/utils/Date.kt
@@ -12,10 +12,18 @@ import java.util.concurrent.TimeUnit
  * @since 2022/7/21 23:41
  */
 fun Date.asWordOrFormattedString(): String {
-    val distance = System.currentTimeMillis() - time
+    val calendar = Calendar.getInstance()
+    val currentCalendar = Calendar.getInstance()
+    calendar.time = this
+    val date = calendar.get(Calendar.DATE)
+    val month = calendar.get(Calendar.MONTH)
+    val year = calendar.get(Calendar.YEAR)
+    val currentDate = currentCalendar.get(Calendar.DATE)
+    val currentMonth = currentCalendar.get(Calendar.MONTH)
+    val currentYear = currentCalendar.get(Calendar.YEAR)
     return when {
-        distance < TimeUnit.DAYS.toMillis(1) -> "Today"
-        distance < TimeUnit.DAYS.toMillis(2) -> "Yesterday"
+        date == currentDate && month == currentMonth && year == currentYear -> "Today"
+        date == currentDate - 1 && month == currentMonth && year == currentYear -> "Yesterday"
         else -> asEnglishString()
     }
 }
